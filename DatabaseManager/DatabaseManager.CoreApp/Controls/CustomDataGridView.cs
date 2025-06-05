5ad4fcbdd2dd55280f02dcf0bc5f96ba52852534
victor-wiki <victor-wiki@hotmail.com>
2025-06-05 20:04:13 +0800

    add "CustomDataGridView"

---
+++ DatabaseManager/DatabaseManager.CoreApp/Controls/CustomDataGridView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DatabaseManager.Controls
+{
+    public class CustomDataGridView: DataGridView
+    {
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            try
+            {
+                base.OnPaint(e);
+            }
+            catch (Exception ex)
+            {
+                this.Invalidate();
+            }
+        }
+    }
+}
